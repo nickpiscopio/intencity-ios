8a27f99a1dff99398a38bfcc3ea9bdbb2db59f26
nickpiscopio <nick.piscopio@gmail.com>
2016-04-26 11:30:40 -0400

    Added the background images for the routines.

--- intencity/RoutineCellController.swift
+++ intencity/RoutineCellController.swift
@@ -13,6 +13,8 @@ class RoutineCellController: UITableViewCell
 {
     @IBOutlet weak var view: IntencityCardView!
     
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     @IBOutlet weak var titleStackView: UIStackView!
     @IBOutlet weak var routineTitle: UILabel!
     @IBOutlet weak var routineDescription: UILabel!
@@ -85,12 +87,15 @@ class RoutineCellController: UITableViewCell
         {
             case RoutineViewController.CUSTOM_ROUTINE_TITLE:
                 view.backgroundColor = Color.secondary_dark
+                backgroundImage.image = UIImage(named: "custom_routine_background")
                 break
             case RoutineViewController.INTENCITY_ROUTINE_TITLE:
                 view.backgroundColor = Color.secondary_light
+                backgroundImage.image = UIImage(named: "intencity_routine_background")
                 break
             case RoutineViewController.SAVED_ROUTINE_TITLE:
                 view.backgroundColor = Color.primary
+                backgroundImage.image = UIImage(named: "saved_routine_background")
                 break
             default:
                 break
